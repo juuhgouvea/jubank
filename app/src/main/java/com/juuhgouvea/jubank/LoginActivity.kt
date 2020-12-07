0c2b1cd1f18acb7be9fa30bb0e8c8f1d2a7c6a73
juuhgouvea <juliasantos752@yahoo.com.br>
2020-12-07 05:08:25 -0300

    remove static login

--- app/src/main/java/com/juuhgouvea/jubank/LoginActivity.kt
+++ app/src/main/java/com/juuhgouvea/jubank/LoginActivity.kt
@@ -1,32 +1,59 @@
 package com.juuhgouvea.jubank
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.juuhgouvea.jubank.database.AppDatabase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    private val USERNAME = "julia";
-    private val PASSWORD = "12345";
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val loggedUserId = getPreferences(Context.MODE_PRIVATE)
+                .getLong("logged_user_id", -1).toInt();
+
+        if(loggedUserId !== -1) {
+            goToMain();
+        }
+
         btnLogin.setOnClickListener { loginHandler(inputUsername.text.toString(), inputPassword.text.toString()) }
+        linkRegister.setOnClickListener { goToRegister() }
     }
 
     private fun loginHandler(username: String = "", password: String = "") {
-        if(!username.toLowerCase().equals(USERNAME) or !password.equals(PASSWORD)) {
+        val userDao = MainApplication.appContext?.let { AppDatabase.getDatabase(it) }?.userDao()
+        val user = userDao?.findByUsernameAndPassword(username, password);
+
+        if(user === null) {
             Toast.makeText(this, R.string.invalid_user, Toast.LENGTH_SHORT)
                 .show();
             return;
         }
 
+        user.id?.let {
+            this.getPreferences(Context.MODE_PRIVATE)
+                .edit()
+                .putLong("logged_user_id", it.toLong())
+                .commit()
+        };
+        goToMain();
+    }
+
+    private fun goToRegister() {
+        var intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun goToMain() {
         var intent = Intent(this, MainActivity::class.java).apply {
             flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         };
+
         startActivity(intent);
     }
 }
--- app/src/main/java/com/juuhgouvea/jubank/RegisterActivity.kt
+++ app/src/main/java/com/juuhgouvea/jubank/RegisterActivity.kt
@@ -1,11 +1,42 @@
 package com.juuhgouvea.jubank
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.juuhgouvea.jubank.database.AppDatabase
+import com.juuhgouvea.jubank.models.User
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnRegister.setOnClickListener { registerHandler(
+            inputUsername.text.toString(),
+            inputPassword.text.toString(),
+            inputConfirmPassword.text.toString()
+        ) }
+    }
+
+    private fun registerHandler(
+        username: String = "",
+        password: String = "",
+        confirmPassword: String = ""
+    ) {
+        if(username.equals("") || password.equals("") || !password.equals(confirmPassword)) {
+            return;
+        }
+
+        val userDao = MainApplication.appContext?.let { AppDatabase.getDatabase(it) }?.userDao();
+
+
+        val newUser = User(username, password);
+        newUser.id = userDao?.insert(newUser);
+
+        var intent = Intent(this, LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        startActivity(intent);
     }
 }
